b4575a16b6b19555447361891429862ee28fc3f1
vladimiremi <vladimiremi2014@gmail.com>
2020-11-19 13:15:43 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Previsão do Tempo</h1>
+          <input type="text" name="" id="" placeholder="Insira o nome da cidade aqui"/>
       </header>
+
+      <main>
+        <h2>Capitais</h2>
+        <div className="cidades">
+          <div className="dados">
+            <p>Min Max</p>
+            <ul>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+            </ul>
+          </div>
+          <div className="dados">
+          <p>Min Max</p>
+            <ul>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+              <li>18º 27º Rio de Janeiro</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }

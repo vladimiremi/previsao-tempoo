7c25b7d79c9dc72a57029207dd762eb810b5763a
vladimiremi <vladimiremi2014@gmail.com>
2020-11-20 06:39:54 -0300

    Fx: center page

--- src/App.js
+++ src/App.js
@@ -4,35 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <h1>Previsão do Tempo</h1>
-          <input type="text" name="" id="" placeholder="Insira o nome da cidade aqui"/>
-      </header>
+      <div className="tudo">
+        
+    
+        <header className="App-header">
+            <h1>Previsão do Tempo</h1>
+            <input type="text" name="" id="" placeholder="Insira o nome da cidade aqui"/>
+        </header>
 
-      <main>
-        <h2>Capitais</h2>
-        <div className="cidades">
-          <div className="dados">
-            <p>Min Max</p>
-            <ul>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-            </ul>
+        <section>
+          <div className="capitais">
+            <h2>Capitais</h2>
+            <div className="cidades">
+              <div className="dados">
+                <p>Min Max</p>
+                <ul>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                </ul>
+              </div>
+              <div className="dados">
+              <p>Min Max</p>
+                <ul>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                  <li>18º 27º Rio de Janeiro</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="dados">
-          <p>Min Max</p>
-            <ul>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-              <li>18º 27º Rio de Janeiro</li>
-            </ul>
-          </div>
-        </div>
-      </main>
-
+        </section>
+        <footer><br/> Com amor Mimi 😍😍</footer>
+       </div>
     </div>
   );
 }
